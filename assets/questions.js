6035c7d9a47d7092b28f7a5548a3452bfbbcd558
McNeace <mcneaceseth15@gmail.com>
2020-03-07 22:09:57 -0500

    Close

---
+++ assets/questions.js
@@ -0,0 +1,34 @@
+var questions = [{
+    question: "What does CSS stand for?",
+    option1: "Compact Style Sheet",
+    option2: "Computer Screen Sheet",
+    option3: "Complex Style Sheet",
+    option4: "Cascading Style Sheet",
+    answer: "3"
+},
+{
+    question: "What kind of kung-fu is used the most, while being a devoloper?",
+    option1: "Baguazhang",
+    option2: "Google Fu",
+    option3: "Tai Chi",
+    option4: "Wing Chun",
+    answer: "3"
+},
+{
+    question: "Which one of these things is not a part of being a devoloper?",
+    option1: "Confussion",
+    option2: "Rage moments",
+    option3: "Crying",
+    option4: "Joy",
+    answer: "4"
+},
+
+{
+    question: "How many lauguages are in code?",
+    option1: "1",
+    option2: "4",
+    option3: "15",
+    option4: "700",
+    answer: "4"
+},
+]
